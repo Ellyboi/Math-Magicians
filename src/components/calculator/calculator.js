c1566792ae36bb305f8601eb9bbfdd6e3e1051e0
ELIJAH <vitoesitemitope@gmail.com>
2023-04-26 19:53:12 +0100

    components added

---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Button from '../button/Button';
+import styles from './calculator.module.css';
+
+const Calculator = () => {
+  const [result, setResult] = useState(0);
+
+  const handleClick = (ev) => {
+    setResult((prevState) => calculate(prevState, ev.target.innerText));
+  };
+  return (
+    <div className={styles.calculatorWrapper}>
+      <div className={styles.display}>{result.next || result.total || 0}</div>
+      <div className={styles.row}>
+        <Button symbole="AC" handleClick={handleClick} />
+        <Button symbole="+/-" handleClick={handleClick} />
+        <Button symbole="%" handleClick={handleClick} />
+        <Button symbole="÷" className="operatorBtn" handleClick={handleClick} />
+      </div>
+      <div className={styles.row}>
+        <Button symbole="7" handleClick={handleClick} />
+        <Button symbole="8" handleClick={handleClick} />
+        <Button symbole="9" handleClick={handleClick} />
+        <Button symbole="x" className="operatorBtn" handleClick={handleClick} />
+      </div>
+      <div className={styles.row}>
+        <Button symbole="4" handleClick={handleClick} />
+        <Button symbole="5" handleClick={handleClick} />
+        <Button symbole="6" handleClick={handleClick} />
+        <Button symbole="-" className="operatorBtn" handleClick={handleClick} />
+      </div>
+      <div className={styles.row}>
+        <Button symbole="1" handleClick={handleClick} />
+        <Button symbole="2" handleClick={handleClick} />
+        <Button symbole="3" handleClick={handleClick} />
+        <Button symbole="+" className="operatorBtn" handleClick={handleClick} />
+      </div>
+      <div className={styles.row}>
+        <Button symbole="0" className="zeroBtn" handleClick={handleClick} />
+        <Button symbole="." handleClick={handleClick} />
+        <Button symbole="=" className="operatorBtn" handleClick={handleClick} />
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
