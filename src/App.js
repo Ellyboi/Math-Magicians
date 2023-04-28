5f0263d19b33bc839794d330521d5de5a680b868
ELIJAH <vitoesitemitope@gmail.com>
2023-04-28 21:54:28 +0100

    update to use api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import CalculatorPage from './pages/CalculatorPage/CalculatorPage';
-import Home from './pages/Home/Home';
-import Quote from './pages/Quote/Quote';
+import './App.css';
+import Calculator from './Components/Calculator';
+import Quotes from './Components/Quotes';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
-    </div>
+    <>
+      <Calculator />
+      <Quotes />
+    </>
   );
 }
 
---
+++ src/Components/Calculator.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+  const [result, setResult] = useState({ total: 0, next: null, operation: null });
+
+  const handleKeyPress = (clickButton) => {
+    setResult((prevState) => {
+      const calculator = calculate(prevState, clickButton.target.textContent);
+      return { ...prevState, ...calculator };
+    });
+  };
+
+  return (
+    <>
+      <div className="calculator">
+        <p className="outputResult">
+          { result.total }
+          { result.operation }
+          { result.next }
+        </p>
+        <div className="buttonContainer">
+          {buttons.map((button) => (
+            <button type="button" className="calcButtons" key={button} onClick={handleKeyPress}>{button}</button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Calculator;
